3c1a03014df2426757e26fb6f179ede317afa986
dowan <dowan@laptop.dowan.ovh>
2015-09-11 10:27:26 +0200

    correction des conflits

--- src/p2p/ServeurTCP.java
+++ src/p2p/ServeurTCP.java
@@ -29,7 +29,6 @@ public class ServeurTCP {
 				e.printStackTrace();
 				System.exit(1);
 			}
-<<<<<<< HEAD
 		
 			realiseService(unClient);
 		}
@@ -50,9 +49,7 @@ public class ServeurTCP {
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(1);
-=======
 			new ThreadServeurTCP(unClient).run();
->>>>>>> f0db53f57f0eefab4275cd62bd4b6386d323a742
 		}
 	}
 
