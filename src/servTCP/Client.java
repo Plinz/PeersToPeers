ade7ce5c01afa7474142040abe2bceb1ff36da31
Antoine DUQUENNOY <antoine.duquennoy@etudiant.univ-lille1.fr>
2015-09-16 23:31:39 +0200

    yolo

--- src/servTCP/Client.java
+++ src/servTCP/Client.java
@@ -5,11 +5,9 @@ import java.io.*;
 
 public class Client {
 	static Socket s;
-	private File fichier;
 
 	public Client(String ip, int port, File fichier) throws UnknownHostException, IOException {
 		s = new Socket(ip, port);
-		this.fichier = fichier;
 		ThreadClient c = new ThreadClient(s, fichier);
 		c.run();
 		s.close();
--- src/servTCP/Serveur.java
+++ src/servTCP/Serveur.java
@@ -9,11 +9,9 @@ import java.net.UnknownHostException;
 public class Serveur{
     static Socket sock;
     static ServerSocket sv;
-    private File fichier;
     public Serveur(int port, File fichier) throws UnknownHostException, IOException {
     	this.sv = new ServerSocket(port);
     	this.sock = sv.accept();
-    	this.fichier = fichier;
     	ThreadServeur ts = new ThreadServeur(sock, fichier);
     	ts.run();  
     	sock.close();
